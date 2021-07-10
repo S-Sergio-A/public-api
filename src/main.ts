d0deddbd0707b199a9077616d3764d1c4ac4768a
Sergey <sergios33033@gmail.com>
2021-07-10 18:24:06 +0300

    Added "reflect-metadata" import.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import helmet from 'helmet';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
+import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,18 +10,18 @@ async function bootstrap() {
   app.enableCors({
     origin: [process.env.FRONT_URL],
     credentials: true,
-    exposedHeaders: ['Access-Token', 'Refresh-Token', 'Client-Token', 'Country', 'Content-Type'],
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS']
+    exposedHeaders: ["Access-Token", "Refresh-Token", "Client-Token", "Country", "Content-Type"],
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"]
   });
 
   const config = new DocumentBuilder()
-    .setTitle('Authentication and entrance microservice')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('authentication, authorization, entrance')
+    .setTitle("Authentication and entrance microservice")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .addTag("authentication, authorization, entrance")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(3000);
 }
