2891e9e3f2e58ac40167b0c9b1680ae05c040557
Sergey <sergios33033@gmail.com>
2021-07-10 18:21:45 +0300

    Added authentication service and middleware.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,23 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
-// import { UserAuthenticationMiddleware } from './middlewares/entrance.authentication.middleware';
-// import { UserController } from './entrance/entrance.controller';
-import { ValidationModule } from "./pipes/validation.module";
-// import { AuthModule } from './auth/auth.module';
-import { EntranceModule } from "./entrance/entrance.module";
 import { EntranceController } from "./entrance/entrance.controller";
+import { ValidationModule } from "./pipes/validation.module";
+import { EntranceModule } from "./entrance/entrance.module";
+import { AuthModule } from "./auth/auth.module";
+import { ClientAuthMiddleware } from "./middlewares/client.authentication.middleware";
+import { UserAuthenticationMiddleware } from "./middlewares/user.authentication.middleware";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ThrottlerModule.forRoot({
-      ttl: 300,
-      limit: 10,
+      ttl: 1000,
+      limit: 40,
       ignoreUserAgents: [new RegExp("googlebot", "gi"), new RegExp("bingbot", "gi")]
     }),
+    AuthModule,
     EntranceModule,
     ValidationModule
   ],
@@ -27,14 +30,17 @@ import { EntranceController } from "./entrance/entrance.controller";
   ]
 })
 export class AppModule {
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(UserAuthenticationMiddleware)
-  //     .exclude(
-  //       { path: '/entrance/sign-up', method: RequestMethod.POST },
-  //       { path: '/entrance/forgot-password', method: RequestMethod.POST },
-  //       { path: '/entrance/forgot-password-verify', method: RequestMethod.PUT }
-  //     )
-  //     .forRoutes(UserController);
-  // }
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ClientAuthMiddleware)
+      .forRoutes({ path: "client/contact", method: RequestMethod.ALL })
+      .apply(UserAuthenticationMiddleware)
+      .forRoutes(
+        { path: "/entrance/email/:id", method: RequestMethod.PUT },
+        { path: "/entrance/password/:id", method: RequestMethod.PUT },
+        { path: "/entrance/phone/:id", method: RequestMethod.PUT },
+        { path: "/entrance/optional/:id", method: RequestMethod.PUT },
+        { path: "/entrance/refresh-session", method: RequestMethod.GET }
+      );
+  }
 }
--- src/middlewares/client.authentication.middleware.ts
+++ src/middlewares/client.authentication.middleware.ts
@@ -1,27 +1,19 @@
-// import { HttpException } from "@nestjs/common/exceptions/http.exception";
-// import { NestMiddleware, HttpStatus, Injectable } from "@nestjs/common";
-// import { Request, Response, NextFunction } from "express";
-// import { AuthService } from "../auth/services/auth.service";
-// import { ClientService } from "../client/client.service";
-//
-// @Injectable()
-// export class ClientAuthMiddleware implements NestMiddleware {
-//   constructor(private readonly authService: AuthService, private readonly clientService: ClientService) {}
-//
-//   async use(req: Request, res: Response, next: NextFunction) {
-//     const clientToken = req.headers["client-token"];
-//
-//     if (clientToken) {
-//       await this.authService.verifyClientsToken(req);
-//       const clientId = req.client.clientId;
-//       const client = await this.clientService._findById(clientId);
-//
-//       if (!client) {
-//         throw new HttpException("Please, reload the page.", HttpStatus.UNAUTHORIZED);
-//       }
-//       next();
-//     } else {
-//       throw new HttpException("Please, reload the page.", HttpStatus.UNAUTHORIZED);
-//     }
-//   }
-// }
+import { NestMiddleware, HttpStatus, Injectable, HttpException } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+import { AuthService } from "../auth/auth.service";
+
+@Injectable()
+export class ClientAuthMiddleware implements NestMiddleware {
+  constructor(private readonly authService: AuthService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const clientToken = req.headers["client-token"];
+
+    if (clientToken) {
+      await this.authService.verifyClientsToken(req);
+      next();
+    } else {
+      throw new HttpException("Please, reload the page.", HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
