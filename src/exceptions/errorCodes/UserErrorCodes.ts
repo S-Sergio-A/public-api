e4bead3f22c1e92fa1c3dac6cda3f3eea011408e
Sergey <sergios33033@gmail.com>
2021-07-12 17:25:17 +0300

    Unnecessary file removed.

--- src/exceptions/errorCodes/UserErrorCodes.ts
+++
@@ -1,9 +0,0 @@
-export class UserErrorCodes {
-  static readonly USER_NOT_FOUND = new UserErrorCodes('USER_NOT_FOUND', 801, "This field mustn't be empty.");
-
-  private constructor(private readonly key: string, public readonly code: number, public readonly value: string) {}
-
-  toString() {
-    return this.key;
-  }
-}
