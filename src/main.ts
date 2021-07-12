d3f301c5172a3761b88078e3c48c671cd5ee1f94
Sergey <sergios33033@gmail.com>
2021-07-12 17:42:35 +0300

    Minor updates.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestFactory } from "@nestjs/core";
 import helmet from "helmet";
 import "reflect-metadata";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors({
-    origin: [process.env.FRONT_URL],
+    origin: [process.env.FRONT_URL, "*"],
     credentials: true,
     exposedHeaders: ["Access-Token", "Refresh-Token", "Client-Token", "Country", "Content-Type"],
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"]
   });
 
   const config = new DocumentBuilder()
-    .setTitle("Authentication and entrance microservice")
+    .setTitle("Authentication and public microservice")
     .setDescription("The cats API description")
     .setVersion("1.0")
-    .addTag("authentication, authorization, entrance")
+    .addTag("authentication, authorization, public")
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
