9b575b9791a37a144a908643881c202f42df7156
Sergey <sergios33033@gmail.com>
2021-07-12 17:33:47 +0300

    Unnecessary code removed.

--- src/exceptions/filters/RequestBodyAndInternal.exception-filter.ts
+++ src/exceptions/filters/RequestBodyAndInternal.exception-filter.ts
@@ -18,18 +18,3 @@ export class RequestBodyAndInternalExceptionFilter implements ExceptionFilter {
     });
   }
 }
-
-// @Injectable()
-// export class NotFoundInterceptor implements NestInterceptor {
-//   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-//     // next.handle() is an Observable of the controller's result value
-//     return next.handle()
-//       .pipe(catchError(error => {
-//         if (error instanceof EntityNotFoundError) {
-//           throw new NotFoundException(error.message);
-//         } else {
-//           throw error;
-//         }
-//       }));
-//   }
-// }
--- src/exceptions/filters/Validation.exception-filter.ts
+++ src/exceptions/filters/Validation.exception-filter.ts
@@ -16,18 +16,3 @@ export class ValidationExceptionFilter implements ExceptionFilter {
     });
   }
 }
-
-// @Injectable()
-// export class NotFoundInterceptor implements NestInterceptor {
-//   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-//     // next.handle() is an Observable of the controller's result value
-//     return next.handle()
-//       .pipe(catchError(error => {
-//         if (error instanceof EntityNotFoundError) {
-//           throw new NotFoundException(error.message);
-//         } else {
-//           throw error;
-//         }
-//       }));
-//   }
-// }
