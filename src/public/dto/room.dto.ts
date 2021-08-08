307c9bf0f0b8dd3dfea01b0e5f3061a821510e62
Sergey <sergios33033@gmail.com>
2021-08-08 16:50:27 +0300

    Photo property added.

--- src/public/dto/room.dto.ts
+++ src/public/dto/room.dto.ts
@@ -1,82 +1,50 @@
-import { IsDefined, IsNotEmpty, IsString, IsNumber, IsArray, IsBoolean } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsDefined, IsNotEmpty, IsString, IsNumber, IsArray, IsBoolean, IsOptional, IsUrl } from "class-validator";
+import { Types } from "mongoose";
 
 export class RoomDto {
-  @ApiProperty({
-    example: "Saddam Hussein's Cave",
-    description: "Name of the chat.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsString()
   name: string;
 
-  @ApiProperty({
-    example: "Я продаю лучшие хергитские гобелены. Не желаете взглянуть?",
-    description: "The description of the chat.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsString()
-  description: string;
+  @IsOptional()
+  description?: string;
+  
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  photo?: string;
 
-  @ApiProperty({
-    example: "false",
-    description: "Is it a personal one-to-one chat or a group chat.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsBoolean()
   isUser: boolean;
 
-  @ApiProperty({
-    example: "false",
-    description: "Is it a private chat room.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsBoolean()
   isPrivate: boolean;
 
-  @ApiProperty({
-    example: "[fgf123d123, hgh234fgf, 23h56h68jk ...]",
-    description: "The list of users which have entered the chat.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsArray()
-  usersID: string[];
+  usersID: Types.ObjectId[];
 
-  @ApiProperty({
-    example: "[fgf123d123, hgh234fgf, 23h56h68jk ...]",
-    description: "The list of messages that have been sent in the chat.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsArray()
-  messagesID: string[];
+  messagesID: Types.ObjectId[];
 
-  @ApiProperty({
-    example: "16",
-    description: "The number of current members.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsNumber()
   membersCount: number;
 
-  @ApiProperty({
-    example: "13:15 06.07.2021",
-    description: "The timestamp of the chat creation.",
-    format: "string"
-  })
   @IsDefined()
   @IsNotEmpty()
   @IsString()
