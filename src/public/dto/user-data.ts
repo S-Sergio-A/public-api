6ecd4b056447c7ee5251fe42a038437b4926b965
Sergey <sergios33033@gmail.com>
2021-08-09 13:29:09 +0300

    New DTOs added.

---
+++ src/public/dto/user-data.ts
@@ -0,0 +1,12 @@
+export interface UserData {
+  user: {
+    _id: string;
+    email: string;
+    username: string;
+    phoneNumber: string;
+    firstName: string;
+    lastName: string;
+    birthday: string;
+    photo: string;
+  };
+}
