983523a088b56cefe147e4d26c3d67dbd2c74725
s.smovzh <sergios33033@gmail.com>
2024-12-15 16:49:44 +0200

    fix: update lib usage

--- src/modules/common/config/configuration.ts
+++ src/modules/common/config/configuration.ts
@@ -1,31 +1,34 @@
 import * as process from "process";
 import * as dotenv from "dotenv";
+import { AppConfigInterface, MongoConfigInterface, RabbitConfigInterface, TokenConfigInterface } from "@ssmovzh/chatterly-common-utils";
+
 dotenv.config();
 
 export default () => ({
   app: {
-    port: process.env.PORT,
+    port: +process.env.PORT,
     environment: process.env.ENVIRONMENT,
     clientUrl: process.env.CLIENT_URL
-  },
+  } as AppConfigInterface,
   mongo: {
     username: process.env.MONGO_USERNAME,
     password: process.env.MONGO_PASSWORD,
     clusterUrl: process.env.MONGO_CLUSTER_URL,
     database: process.env.MONGO_DATABASE_NAME
-  },
+  } as MongoConfigInterface,
   rabbitConfig: {
     protocol: "amqp",
     hostname: process.env.RABBIT_HOST,
     port: process.env.RABBIT_PORT ? +process.env.RABBIT_PORT : 5672,
     username: process.env.RABBIT_USERNAME,
     password: process.env.RABBIT_PASSWORD
-  },
+  } as RabbitConfigInterface,
   jwt: {
     secret: process.env.JWT_SECRET,
-    clientsSecret: process.env.CLIENTS_JWT_SECRET,
+    clientSecret: process.env.CLIENTS_JWT_SECRET,
     refreshSecret: process.env.JWT_REFRESH_SECRET,
-    expirationTime: process.env.JWT_EXPIRATION_TIME,
-    refreshExpirationTime: process.env.JWT_REFRESH_EXPIRATION_TIME
-  }
+    expiresIn: process.env.JWT_EXPIRATION_TIME,
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRATION_TIME,
+    longExpiresIn: process.env.JWT_EXPIRATION_TIME_LONG
+  } as TokenConfigInterface
 });
